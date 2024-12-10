655a08ad49b72f38b39e8cf2cd8532a73b1979fc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-09 23:49:57 -0500

    created massDel function

--- src/resources/store.ts
+++ src/resources/store.ts
@@ -5,6 +5,16 @@
     export let tasks = $state<task[]>([])
 
 //FUNCTIONS
-    export const add = (newTask: task) => {
+    export const add = (newTask: task) => { // adds new task to tasks
         tasks.push(newTask);
+    }
+    export const del = (task: string) => { // deletes task from tasks
+        tasks = tasks.filter(t => {
+            return t.task !== task
+        })
+    }
+    export const massDel = () => {
+        tasks = tasks.filter(t => {
+            return t.done !== true;
+        })
     }
