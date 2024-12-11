b2a1c4768cc50072ac9cdacacbd902852ec737d0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-10 22:13:23 -0500

    created intiial styles for control panel

--- src/components/controls.svelte
+++ src/components/controls.svelte
@@ -2,14 +2,26 @@
 
 </script>
 
-<div>
-    <input type=text placeholder="add a task" />
-    <div>
+<div class="container">
+    <input type=text placeholder="add a task..." />
+    <div class="controls">
         <button>Add</button>
         <button>Delete All</button>
     </div>
 </div>
 
 <style>
-
+    .container {
+        display: flex;
+        justify-content: space-between;
+        height: 2.5rem;
+    }
+    .controls {
+        display: flex;
+        width: 15%
+    }
+    input {
+        width: 85%;
+        padding: 1rem;
+    }
 </style>
