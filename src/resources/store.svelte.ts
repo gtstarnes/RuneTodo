52f7b25bcb2a6113d2d1e152702e86c6a5e16c9c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-10 22:41:43 -0500

    removed functions

--- src/resources/store.svelte.ts
+++ src/resources/store.svelte.ts
@@ -4,21 +4,5 @@
 //VARIABLES
     let tasks = $state<Task[]>([])
 
-//FUNCTIONS
-    const add = (t: string) => { // adds new task to tasks
-        const newTask = {task: t, done: false, edit: false}
-        tasks.push(newTask);
-    }
-    const del = (task: string) => { // deletes task from tasks
-        tasks = tasks.filter(t => {
-            return t.task !== task
-        })
-    }
-    const massDel = () => { // deletes all completed tasks
-        tasks = tasks.filter(t => {
-            return t.done !== true;
-        })
-    }
-
 //EXPORTS
-export {tasks, add, del, massDel}
+export {tasks}
