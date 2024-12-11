e4e2964b5943b66d3c6e8bb2b28a1d24178fb66a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-10 22:21:41 -0500

    added basic task row

--- src/components/tasks/task.svelte
+++ src/components/tasks/task.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+// IMPORTS
+	import type { Task } from "../../resources/types";
 
+// PROPS
+    let {task = "", done = false, edit = false}: Task = $props();
 </script>
 
 <div>
-
+    <input type="checkbox" bind:checked={done} />
+    <div>{task}</div> 
+    <button>Del</button>
 </div>
 
 <style>
-    
+
 </style>
