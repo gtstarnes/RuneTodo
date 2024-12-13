2be81cd121e47d50f04f89ffc949b6e505ec26d6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-13 01:24:36 -0500

    created conditional rendering for display indivdual tasks

--- src/components/tasks/taskList.svelte
+++ src/components/tasks/taskList.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+	import { tasks } from "../../resources/store.svelte";
+	import Task from "./task.svelte";
+
 
 </script>
 
 <div class="container">
-
+    {#if tasks.length === 0}
+        <div>Task List is empty</div>
+    {:else}
+        {#each tasks as task (task.task)}
+            <Task {...task} />
+        {/each}
+    {/if}
 </div>
 
 <style>
