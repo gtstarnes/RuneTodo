b87fb2ab434bd3a8ee0cd056faf84241219dd1e5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-13 02:38:41 -0500

    imported del function

--- src/components/tasks/task.svelte
+++ src/components/tasks/task.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 // IMPORTS
 	import type { Task } from "../../resources/types";
+    import { del } from "../../resources/store.svelte";
 
 // PROPS
     let {task = "", done = false, edit = false}: Task = $props();
@@ -9,7 +10,7 @@
 <div>
     <input type="checkbox" bind:checked={done} />
     <div>{task}</div> 
-    <button>Del</button>
+    <button onclick={()=>del(task)}>Del</button>
 </div>
 
 <style>
