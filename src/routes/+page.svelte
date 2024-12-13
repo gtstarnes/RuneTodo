f52a5c75341715180fe1f36ce34412868952afb2
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-13 01:23:48 -0500

    added tasklist to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Controls from "../components/controls.svelte";
+	import TaskList from "../components/tasks/taskList.svelte";
 
 
 </script>
 
 <section>
     <Controls />
+    <TaskList />
 </section>
 
 <style>
